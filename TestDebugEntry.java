c0361e9b08f22938d2cf3277de1dd8c61085a699
Adam Vander Pas <azvanderpas@gmail.com>
2024-08-24 15:54:05 -0500

    Update paths

--- TestDebugEntry.java
+++ TestDebugEntry.java
@@ -7,7 +7,7 @@ import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
-import frc.robot.utilities.DebugEntry;
+import frc.robot.common.DebugEntry;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
